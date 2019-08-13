14c0336f9dfdbec0b6d31981be00fa4fb62b8811
yuriykis <yurakis9@gmail.com>
2019-08-13 23:11:34 +0200

    Added view and entity detail

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.base_list, name='base_list'),
+    path('osoba/<int:pk>/', views.entity_detail, name='entity_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Osoba
 
 # Create your views here.
 def base_list(request):
     osoby = Osoba.objects.all()
     return render(request, 'myapp/base_list.html', {'osoby': osoby})
+
+def entity_detail(request, pk):
+   osoba = get_object_or_404(Osoba, pk=pk)
+   return render(request, 'myapp/entity_detail.html', {'osoba': osoba})
