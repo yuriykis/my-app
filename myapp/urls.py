a2a5d346d3798444d74dd0ba21f2f3f34844face
yuriykis <yurakis9@gmail.com>
2019-08-14 23:20:59 +0200

    Added deleting persons

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.base_list, name='base_list'),
     path('osoba/<int:pk>/', views.entity_detail, name='entity_detail'),
     path('osoba/new', views.osoba_new, name='osoba_new'),
+    path('osoba/<int:pk>/edit', views.osoba_edit, name='osoba_edit'),
+    path('osoba/<int:pk>/detele>', views.osoba_delete, name='osoba_delete'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -22,3 +22,23 @@ def osoba_new(request):
     else:
         form = Osoba_add()
     return render(request, 'myapp/osoba_edit.html', {'form': form})
+
+def osoba_edit(request, pk):
+    osoba = get_object_or_404(Osoba, pk=pk)
+    if request.method == "POST":
+        form = Osoba_add(request.POST, instance=osoba)
+        if form.is_valid():
+            osoba = form.save()
+            osoba.save()
+            return redirect('entity_detail', pk=osoba.pk)
+    else:
+       form=Osoba_add(instance=osoba)
+    return render(request, 'myapp/osoba_edit.html', {'form': form})
+
+
+def osoba_delete(request, pk):
+    osoba = get_object_or_404(Osoba, pk=pk)
+    osoba.delete()
+    osoby = Osoba.objects.all()
+    return render(request, 'myapp/base_list.html', {'osoby': osoby})
+
